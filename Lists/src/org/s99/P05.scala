5005b63209810a8b24ec5a475b15ef6401b43512
laviniafr <laviniafratila@yahoo.com>
2022-04-13 21:22:24 +0100

    Added comments, reformatted

--- Lists/src/org/s99/P05.scala
+++ Lists/src/org/s99/P05.scala
@@ -11,6 +11,7 @@ import scala.annotation.tailrec
  */
 object P05 {
 
+  // my first version with take and drop to check both halves of a list
   def isPalindrome(list: List[Int]): Boolean = {
     if (list.length % 2 != 0)
       list.take(list.length / 2).reverse equals list.drop(list.length / 2 + 1)
@@ -18,16 +19,19 @@ object P05 {
       list.take(list.length / 2).reverse equals list.drop(list.length / 2)
   }
 
+  // my second version with pattern matching
+  // if first and last elements of a list are equal, a recursive call
+  // is made using the list excluding the equal margins
   @tailrec
   def isPal[T](list: List[T]): Boolean = list match {
-    case h :: Nil => true
+    case _ :: Nil => true
     case h :: t if h == t.last => isPal(list.slice(1, list.length - 1))
     case _ => false
   }
 
   def main(args: Array[String]): Unit = {
-    var list = List(1, 2, 3, 2, 1)
-    //    println(isPalindrome(list))
+    val list = List(1, 2, 3, 2, 1)
+    println(isPalindrome(list))
     println(isPal(list))
   }
 }

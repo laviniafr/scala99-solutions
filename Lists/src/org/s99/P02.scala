0858902f871b0932c7573de139d4346f64a78aaa
laviniafr <laviniafratila@yahoo.com>
2022-04-13 20:46:14 +0100

    Added built-in solution, reformatted

--- Lists/src/org/s99/P02.scala
+++ Lists/src/org/s99/P02.scala
@@ -3,13 +3,17 @@ package org.s99
 import scala.annotation.tailrec
 
 /*
-P02 Find the last but one element of a list.
+P02(*) Find the last but one element of a list.
   Example:
   scala> penultimate(List(1, 1, 2, 3, 5, 8))
   res0: Int = 5
  */
 object P02 {
 
+  // my version, using built-ins
+  def penultimate[T](list: List[T]): T = list.take(list.length - 1).last
+
+  // scala99 version with pattern matching
   @tailrec
   def findLastButOne[A](list: List[A]): A = list match {
     case pen :: _ :: Nil => pen
@@ -20,7 +24,8 @@ object P02 {
   def main(args: Array[String]): Unit = {
     val list = List(1, 1, 2, 3, 5, 8)
     //using builtin
-    println(list.take(list.length - 1).last)
+    println(penultimate(list))
+    //using scala99
     println(findLastButOne(list))
   }
 

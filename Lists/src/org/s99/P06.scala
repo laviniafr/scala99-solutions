ac2ce996b85ae74cfeb90c7909acf79319935697
laviniafr <laviniafratila@yahoo.com>
2022-04-13 21:25:21 +0100

    Reformatted

--- Lists/src/org/s99/P06.scala
+++ Lists/src/org/s99/P06.scala
@@ -7,13 +7,14 @@ package org.s99
   res0: List[Int] = List(8, 5, 3, 2, 1, 1)
  */
 object P06 {
+
   // my version
   def rev[T](list: List[T]): List[T] = list match {
     case h :: Nil => List(h)
     case h :: t => t.last :: rev(list.take(list.length - 1))
   }
-  // o(n2) version from scala99
 
+  // o(n2) version from scala99
   def revRecursive[T](list: List[T]): List[T] = list match {
     case Nil => Nil
     case head :: tail => revRecursive(tail) ::: List(head)

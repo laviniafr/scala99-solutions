9d6d9bf108a620d4ac471c21a2310838215b3aa5
laviniafr <laviniafratila@yahoo.com>
2022-04-13 20:42:12 +0100

    Added built-in solution, reformatted

--- Lists/src/org/s99/P01.scala
+++ Lists/src/org/s99/P01.scala
@@ -3,12 +3,15 @@ package org.s99
 import scala.annotation.tailrec
 
 /*
-  P01 Find the last element of a list.
+  P01(*) Find the last element of a list.
   Example:
   scala> last(List(1, 1, 2, 3, 5, 8))
   res0: Int = 8
  */
 object P01 {
+
+  // my solution, using built-in
+  def findLastBuiltin[T](list: List[T]): T = list.last
 
   // scala99 solution
   // The functional approach is to recurse down the list until we hit the end.
@@ -20,9 +23,9 @@ object P01 {
   }
 
   def main(args: Array[String]): Unit = {
-    val list: List[Int] = List(1,1,2,3,5,8)
+    val list: List[Int] = List(1, 1, 2, 3, 5, 8)
     // using builtin
-    println(list.last)
+    println(findLastBuiltin(list))
     // using rec
     println(findLast(list))
   }

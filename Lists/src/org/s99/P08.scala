7782072e2e7735cf65a4b9f108d14f71ac23fe04
laviniafr <laviniafratila@yahoo.com>
2022-04-14 11:19:38 +0100

    Reformatted and added own version

--- Lists/src/org/s99/P08.scala
+++ Lists/src/org/s99/P08.scala
@@ -1,5 +1,7 @@
 package org.s99
 
+import scala.annotation.tailrec
+
 /*
 P08 (**) Eliminate consecutive duplicates of list elements.
 If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
@@ -10,10 +12,17 @@ res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
  */
 object P08 {
 
+  // my version with pattern matching
   def eliminate[T](list: List[T]): List[T] = list match {
     case Nil => Nil
-    case h :: tail => h :: eliminate(tail.dropWhile(_ == h))
-    //    case h::tail => h::tail
+    case h :: e :: tail if h == e => eliminate(h :: tail)
+    case h :: tail => h :: eliminate(tail)
+  }
+
+  // using scala99 version
+  def eliminate1[T](list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case h :: tail => h :: eliminate1(tail.dropWhile(_ == h))
   }
 
   def main(args: Array[String]): Unit = {

d5fe725262fa85ce621dcf8d90f38684a0645af2
laviniafr <laviniafratila@yahoo.com>
2022-04-14 11:00:24 +0100

    Reformatted and added own version

--- Lists/src/org/s99/P07.scala
+++ Lists/src/org/s99/P07.scala
@@ -8,8 +8,16 @@ package org.s99
  */
 object P07 {
 
-  def flatten(list: List[Any]): List[Any] = list flatMap {
-    case li: List[_] => flatten(li)
+  //my attempt with pattern matching
+  def flatten[T](list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case (h: List[T]) :: tail => flatten(h) ::: flatten(tail) //if head is also a list, flatten the head and the tail
+    case h :: tail => h :: flatten(tail) // if h is not a list just flatten the tail
+  }
+
+  // scala99 version with flatmap
+  def flatten1(list: List[Any]): List[Any] = list.flatMap {
+    case li: List[_] => flatten1(li)
     case e => List(e)
   }
 

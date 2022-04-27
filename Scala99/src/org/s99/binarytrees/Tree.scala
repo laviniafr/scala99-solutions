4b776586db7bfa9b54a9ea74d1349a128aeb3a1c
laviniafr <laviniafratila@yahoo.com>
2022-04-27 14:51:19 +0100

    Added the addValue method

--- Scala99/src/org/s99/binarytrees/Tree.scala
+++ Scala99/src/org/s99/binarytrees/Tree.scala
@@ -1,18 +1,38 @@
 package org.s99.binarytrees
 
-import org.s99.arithmetics.Timer
+sealed abstract class Tree[+T]{
+  def addValue[U >: T <% Ordered[U]](x: U): Tree[U]
 
-sealed abstract class Tree[+T]
+}
 
 case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
   override def toString: String = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+
+  override def addValue[U >: T <% Ordered[U]](x: U): Node[U] ={
+    if (x<value) {
+      if(left == End)
+        Node(value, End.addValue(x), right)
+      else
+        Node(value, left.addValue(x),right)
+    }
+    else {
+      if(right==End)
+        Node(value, left, End.addValue(x))
+      else
+        Node(value, left, right.addValue(x))
+    }
+  }
+
 
 }
 
 case object End extends Tree[Nothing] {
   override def toString = "."
+
+  override def addValue[U >: Nothing <% Ordered[U]](x: U): Node[U] = Node(x, End, End)
 }
 
 object Node {
   def apply[T](value: T): Node[T] = Node(value, End, End)
 }
+
